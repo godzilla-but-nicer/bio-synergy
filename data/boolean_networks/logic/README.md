b9d169d54c6f034d5af388326a61c1c3e4eaaa58
godzilla-but-nicer <patgwall@iu.edu>
2021-08-04 19:22:30 -0400

    data share etc

---
+++ data/boolean_networks/logic/README.md
@@ -0,0 +1,15 @@
+# Format Guide
+
+The files in this directory are a weird format called `cnet` used by some SAT
+solver. We don't care about the SAT solver but we do care about transition rule
+logic so we might as well use this. This is a guide to the format pulled from
+the `cana` documentation.
+
+\# .v = number of nodes
+.v 1
+\# .l = node label
+.l 1 node-a
+.l 2 node-b
+\# .n = (node number) (in-degree) (input node 1) … (input node k)
+.n 1 2 4 5
+01 1 # transition rule
--- scripts/get_boolean_networks.py
+++ scripts/get_boolean_networks.py
@@ -9,18 +9,17 @@ from networkx.linalg.graphmatrix import adjacency_matrix
 
 cc_models = bio.load_all_cell_collective_models()
 cc_models.append(bio.DROSOPHILA())
-cc_models.append(bio.BREAST_CANCER())
-cc_models.append(bio.LEUKEMIA())
-cc_models.append(bio.BUDDING_YEAST())
 
 for i, mod in enumerate(cc_models):
     new_name = mod.name.replace(' ', '_').lower()
+
+    # adjacency    
     net = mod.structural_graph()
     adj_mat = nx.to_numpy_array(net)
-
     np.savetxt('data/boolean_networks/adjacency_matrix/' + new_name + '.csv', 
     adj_mat, fmt='%d', delimiter=',')
 
+    # effective graph
     net = mod.effective_graph()
     eff_gph = nx.to_numpy_array(net)
     np.savetxt('data/boolean_networks/effective_graph/' + new_name + '.csv', 
